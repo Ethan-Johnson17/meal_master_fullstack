11c16495e24cbbbcd24a768053bb18d27aaaaf2c
Ethan Johnson <ethan.t.johnson1417@gmail.com>
2023-06-06 15:48:08 -0400

    Tags

--- meal-master-fullstack/Models/Recipe.cs
+++ meal-master-fullstack/Models/Recipe.cs
@@ -4,7 +4,7 @@ public class Recipe
   public int Id { get; set; }
   public string Title { get; set; }
   public string Subtitle { get; set; }
-  public string Category { get; set; }
+  public string Tags { get; set; }
   public string Cuisine { get; set; }
   public string ImgUrl { get; set; }
   public string CreatorId { get; set; }
