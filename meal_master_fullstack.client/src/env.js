632b5cf15bf6a679f78b53a3e14ea38c766eafdd
Ethan Johnson <85658808+Ethan-Johnson17@users.noreply.github.com>
2023-06-14 11:26:47 -0400

    Delete env.js

--- meal_master_fullstack.client/src/env.js
+++
@@ -1,6 +0,0 @@
-export const dev = typeof window !== 'undefined' && window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
-export const useSockets = false
-export const domain = 'lionbolddesigns.us.auth0.com'
-export const clientId = '9mHGBLlYdEzUdJ9ry3Is6b2lHLr6SRXX'
-export const audience = 'https://LionBoldDesigns.com'
