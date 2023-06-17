cb16baea31546da2b775073019c35de0e678230c
Taylor Smith <smith.taylord@gmail.com>
2023-06-17 07:23:14 -0600

    Auth0 working now

--- meal_master_fullstack.client/src/env.js
+++ meal_master_fullstack.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = typeof window !== 'undefined' && window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
 export const domain = 'lionbolddesigns.us.auth0.com'
 export const clientId = '9mHGBLlYdEzUdJ9ry3Is6b2lHLr6SRXX'
