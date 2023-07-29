1f15b6b48b3c0157539253c56f27b19c4943d3b9
ThomF <seniorswanky@icloud.com>
2023-07-29 14:06:23 -0600

    recipe card prototype. design not done

--- meal_master_fullstack.client/src/components/RecipeCard.jsx
+++ meal_master_fullstack.client/src/components/RecipeCard.jsx
@@ -1,16 +1,44 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { recipesService } from '../services/RecipesService';
 import Pop from '../utils/Pop';
 
-export default function RecipeCard() {
-    let [recipeData, setRecipeData] = useState({
-        title: '',
-        subtitle: '',
-        imgUrl: '',
-        cuisine: '',
-        tags: ''
-    });
+export default function RecipeCard({ recipe }) {
+    // NOTE Probably dont need this?? keeping it in case 
+    // let [recipeData, setRecipeData] = useState({
+    //     title: '',
+    //     subtitle: '',
+    //     imgUrl: '',
+    //     cuisine: '',
+    //     tags: ''
+    // });
 
+
+    return (
+        <Link to={`recipe/${recipe.id}`}>
+            <div className='position-relative card-border rounded'>
+                <img
+                    className='img-fluid'
+                    src={recipe.imgUrl}
+                    alt={recipe.title}
+                />
+
+                <div className='position-absolute bottom-0 start-0'>
+                    <div className='ps-2 pt-3 text-body-bg'>
+                        <b>{recipe.title}</b>
+                    </div>
+                    <div className='ps-2'>{recipe.subtitle}</div>
+                    {recipe.tags}
+
+                </div>
+            </div>
+        </Link>
+    );
 
 }
+
+RecipeCard.propTypes = {
+    recipe: PropTypes.object.isRequired,
+};
 
